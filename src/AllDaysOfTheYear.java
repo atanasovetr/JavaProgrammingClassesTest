2c09d843ed622d43dfad5d887910551f81e4367c
Atanasov <ivan.atanasov03@gmail.com>
2020-10-07 19:23:38 +0300

    Test

---
+++ src/AllDaysOfTheYear.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.text.*;
+public class AllDaysOfTheYear {
+    public static void main(String[] args) {
+        SimpleDateFormat pattern = new SimpleDateFormat ("dd.MM.yyyy");
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DATE, 1);
+        c.set(Calendar.MONTH, 0);
+
+        for (int i = 0; i < c.getMaximum(Calendar.DAY_OF_YEAR); i ++){
+        System.out.printf(("%d day of the year: " + pattern.format(c.getTime())),i+1);
+        System.out.println();
+        c.add(Calendar.DATE, 1);
+        }
+    }
+}
